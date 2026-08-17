6d04822e364a5a21962bf29f87f3d28595f4899e
srinu <srinurao1902@gmail.com>
2026-08-17 10:59:56 +0530

    Replace repository with latest local code

--- TRAINING PROJECT/masterdata/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('api/states/', views.get_states, name='api_get_states'),
-    path('api/districts/', views.get_districts, name='api_get_districts'),
-]
